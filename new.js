4eae703cf1310e17c22d32aa624d57df2139536e
saitama-on <130282611+saitama-on@users.noreply.github.com>
2023-12-09 01:29:19 +0530

    Update new.js

--- new.js
+++ new.js
@@ -1,6 +1,5 @@
 const { ApifyClient}= require("apify-client");
 const {ref } = require('firebase/storage');
-const { setMaxoperationretryTime} = require('firebase/storage');
 const {getStorage } = require('firebase/storage');
 const {getDownloadURL } = require('firebase/storage');
 const FirebaseStorage = require('multer-firebase-storage');
@@ -20,49 +19,34 @@ const client = new ApifyClient({
 
 const multer = Multer({
     storage: FirebaseStorage({
-      bucketName: 'newproj2-fc003.appspot.com',
-      directoryPath:'imgs',
+      bucketName: 'sensor-proj-b3fdb.appspot.com',
+      directoryPath:'images',
       credentials: {
-    projectId: "newproj2-fc003",
-    privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC8bjHzFM3x2OXO\nOSSpgu4TVIbCVHBbNm6Nb2fRl8qe9lrZ7gBWMiXw6LqESlYzBmH4PzvjtlxwMjTR\n6GA/kxvMnmne+KtCUHSI248mZJIy/vWzYIw7IK7LsuXeS1wactDZUXxDVFNDHAcV\nq/inJGbBCCWlI8DdCpodDJmAFhbt6E7EshQog8rHfo8egrvHKBfQJwBhaq2wGDJm\nv+IklETFyx3Nb6eYqMhVwjn6+LW9fjlxEp25BBlbyXu+r/52I5FKClpghqwzwFQz\n5ghGnXNgu99d2/Ik+swCdxOm+Kpg5SM6Vys00gcU8swbLTL7xshcs1IwfmzjaFFM\n77E2A1gBAgMBAAECggEAEfnwTXivYhJ3cApfD0XkqRbwCUONeYIyn08gyCvTQoGN\nrstIIHEF0hsdyieeXB1QhOKhd8qqvHBpvhnNbts9tHJrdsBh9EBhMbpCiJ7hHLja\nn1uHXU+zpU7Co8esOIC6IHVn5bN6gwU+PsjKfsWYAPPiRYQo7t2HUNymG0RgOR99\nxHolipddul/c/xwslj3Zohw+oQp+Ir54HMImWUlDMo3ROcLaTyRuN+Fq+2xYVGD7\nG0eEuOqXyP+ZKpT66MOZ+JfZPF+Hrjk2DhtNIDDbl9H11OZt+wKtcCbvB9dxHUO7\n1AYMoGRBLo8t2P/zC4ihQzT6Ytq2WNm7GTeM6nhHjQKBgQDond2mI3aKAGXuyXJ5\nJrRir0fM4A+RUif9wUsc1MN0GUONWZztaYo1AIrMl8tAiN/StVtfcVXnxnSh9ZqV\n2KwRF2RxT3e+12OPIVvxbNOlm7iII5NLsLXFU/ysmmmAaOnuCGAUgYaRfEa1LXIx\n2NmujVU04YG2Eoe5NHZB6i5aRQKBgQDPXz7i/Zfhb+OonVMtXpA7JNP+LDHsQb5C\nIOKXfyTTQtGKeKGJ9gLu1TIRr9BOqBo1ywOTTjVi/YLdz6t5MByflavdXiB68wj+\noOGZqTJ6nNqS0ahvsT2rFXCzhgZD6Ma+LV/E8gHeE2lNKd3tzPwyjtFcgRHFr8zz\n5aJ8txcgjQKBgGcwIHLiqGLvakhS9ITptntQZflsK3u7hiH67OcNOub1jhHV/tcS\nSkzYwLmhXHh0P1WpPa9JB2KZloOB/+EZDLtINTZOmj/+LBwCIFcof3qnaWR644xg\nB0JRz3SByus7nwoIjRaLnrZK+tLEJjQWjfnwmCutHW03Kx3tN8hlqcpBAoGBAK9G\nQOAZGiOor9LXzvL9B4mF8kzWTAf19XrtUrhb9eM+id+xYFA8iHQX7O74HGgvO6AW\nDNqX8xXNUqxql/Ohj7/vAMnlgiyJeSPHFYgMc6chJrmLzgNB2u7c+1oxwN/rQviB\nX34WUy/CVnGrHnKdnNia0CiQy1W85s2LXZBuLHUNAoGBAL8U979jM7jwkgYaNnrw\n2g4rXK2qltwx86U1N4NsS+AtMRQpZU9MExB+B5RNWtqHYq3bYJr3wHevB5rdTd2l\nVAjhebEVG/CC4veNcljVscYqU7vuAbJvzHRMAgeu8SH9WyDAaMq0mpkbWeSQOOfG\nfu/E5q8TyWvYFEAmrR0lCwV6\n-----END PRIVATE KEY-----\n",
-    clientEmail: "firebase-adminsdk-3t5hn@newproj2-fc003.iam.gserviceaccount.com",
-    
+    projectId: "sensor-proj-b3fdb",
+    privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDGQuqTeceRIEMB\n/wKU7ktnYKTAZlVgJBYA+rgk52ct6OcRftUnMfyC7d0xTAfvbPHY3ZAL/b4c03Lm\noJh8ROd+6f3HmZNJ1df2VIxao0DSHitYz5hWM1Toas24DK87wVuOf4ugzJOap2HZ\ntvHk6+QenPl9DIa/OI15s022HYLtWZljnLtmpm7NqvaMpFqRw+DZP8h+fxEc+mfE\nUwT5f9WU/BNgLacR6E5sjfQhIcfM1GnBHKqUB3yOOUG3+xE1fyQhTD3AKuHryPZ7\nQr/2ejAufwKtcV5WrS6U8XD6TENJLd9H3HGzfNp04YzLN/UggvwmFLxZtHBOg3jy\n/1C3/bP3AgMBAAECggEAPECn44vV/5/rXVbvpO1Kky9GwoYXGjG9Uizf+68rp0Mp\nviJqiw/g6rV5THsLylK0qTCkj1BdD4ufVsGltdaG+AYKrzdYEM9hY6ZMRVtp3rF2\ngPYOGNxeyId036NTOfjIZ2URvRuPHCgUT8MOfoDGAhu4oq1E4iZAsKSyUE9ls9XG\nJtmk+s567NqMbXlETxwDLaERhwPJa4hrVzaJea3r5tcrbf30FRb6MI1AjTie/A40\nIYGsI5xgjHKoGjVH7BQZCxNRBIf9Y5gTthm4pMrSoQ5MNEln6wxrr4i/5HxJEr+u\nSwoiWv5oczDK7NZPnvq8bvA7/ZSRPqB9G8jvjrlRUQKBgQD9qYpiE2yKgINRSqSz\nKGc3zwVRj4kJVHm4UBCybbWHFawQbd3sm9FxCOYGyYmzEOIiuAW9bCVnOtSoQ3kR\nNDIjm2hji1XJ8vQ/sbNDZni6cWC83W4/NLWjxDS2nkY+ZFrL4xR+gYPf3UGfP2DN\ndj7IoRXaOTmvSQJIoBvMlWG6+QKBgQDIFqt1yhnM6rfHf7LYXXfucJOMkIYFUvlK\nNo0/otF/PYClJAk9W/Hh1GmjdH+/OZxwwtyGfhV4CqqHAyY2bzJUbWmkQAB5hXdX\n6iATvKb3i9Od/edUfnFBAxdMy2iFGwEnidg3nKU/GxKd5hs+/Xy5lYQ2QR6sxmXu\nI28CUiEybwKBgD9JcHQ9wmPf4mOCywrD9tSuuvX5aMhCIkhA+Zl608KLPq2uq6Lf\n0WZxQOrKl6dse+8LmKsE82QJAoukoe0P576IhIfpeJL1IclObZpQU6i4ptkYKext\nGzBrwHcbkAz6/shFtJRHff55dmjj6CP673kg75HNmz3HamFySN1HG+YJAoGAeK2r\nlixhqUT7uPhc/sBJAGenpkTYgEeOEWObh4EFB9ApUfDNHdPhrEn3uN0f2068+wmc\nBgXvnuoX9bxBhN1GJsOizqzHXIPR+hU8aYsDzRPpe0XTCxQD7KkXmXqpPHaQGK+7\n17bDt671aISLdE9gtXHnPu3BuYUjnyCB6I1udkMCgYEAowoCI8fVnVrY2UNBzyKy\nxJFIVq7P5hwYL6IjRNILmohkzWOp3Ndy8Y67ofjPIkeLHTvDZzoFtI/rzYiAuLf3\n8vpBaGbLKpgGapgWMoVcMhV59jAwafo6Yzn0U8hTGxBs/P7FZ/TjaIuVJfektUa4\nQcRyZJcKnEm4XTP7G/WfGQ4=\n-----END PRIVATE KEY-----\n",
+    clientEmail: "firebase-adminsdk-osb4y@sensor-proj-b3fdb.iam.gserviceaccount.com",
       }
     })
 })
-// const firebaseConfig = {
-//     apiKey: "AIzaSyD2RpxeHhLOMaqzS8-Vo5WuUqLtUFYilUE",
-//     authDomain: "sensor-proj-b3fdb.firebaseapp.com",
-//     projectId: "sensor-proj-b3fdb",
-//     storageBucket: "sensor-proj-b3fdb.appspot.com",
-//     messagingSenderId: "603026668192",
-//     appId: "1:603026668192:web:f6eceb438b31e2b60567c5"
-//   };
 const firebaseConfig = {
-  apiKey: "AIzaSyChK5UYG_whbhl5ulftgElEgwOXM2tEp4g",
-  authDomain: "newproj2-fc003.firebaseapp.com",
-  projectId: "newproj2-fc003",
-  storageBucket: "newproj2-fc003.appspot.com",
-  messagingSenderId: "918494578722",
-  appId: "1:918494578722:web:f12433c8f25bed2c53627b",
-  //measurementId: "G-J7ZJCZL7N5"
-};
-  
+    apiKey: "AIzaSyD2RpxeHhLOMaqzS8-Vo5WuUqLtUFYilUE",
+    authDomain: "sensor-proj-b3fdb.firebaseapp.com",
+    projectId: "sensor-proj-b3fdb",
+    storageBucket: "sensor-proj-b3fdb.appspot.com",
+    messagingSenderId: "603026668192",
+    appId: "1:603026668192:web:f6eceb438b31e2b60567c5"
+  };
+
   // Initialize Firebase
 const app1 = initializeApp(firebaseConfig);
 const storage = getStorage(app1);
-//setMaxOperationRetryTime (1000);
-
 
 app.post("/upload", multer.single("img"), (req,res)=>{
 
-   // res.write("Uploaded successfuly!");
-   console.log("Uploaded successfuly!");
-    
-    //storage.setMaxoperationretryTime(1000);
-    getDownloadURL(ref(storage , 'imgs/' +req.file['originalname']) ).
+    console.log("Uploaded successfuly!");
+
+    getDownloadURL(ref(storage , 'images/' +req.file['originalname'])).
     then((url)=>{
-        console.log("url done");
       const input = {
           "startUrls": [
               {
@@ -75,20 +59,18 @@ app.post("/upload", multer.single("img"), (req,res)=>{
           "maxRequestRetries": 10,
           "debugLog": false
         };
-        
+
         (async () => {
           // Run the Actor and wait for it to finish
           const run = await client.actor("RA7fg2mqciGjZM7hM").call(input);
-      
+
           const { items } = await client.dataset(run.defaultDatasetId).listItems();
-         // res.write("PLEASE WAIT UNTIL REDIRECTED!");
           items.forEach((item) => {
-              
 
               let len_arr = item.ocrText.length;
 
               for(let i=0 ; i<len_arr ;i++) {
-                
+
 
                 if(item.ocrText[i].toLowerCase().includes('exp')){
 
@@ -97,9 +79,9 @@ app.post("/upload", multer.single("img"), (req,res)=>{
                     "query":str1,
                     "lang": "en-US"
                 };
-                
+
                 (async () => {
-                    
+
                     const run = await client.actor("S84ymCuDy3DPGojAH").call(input);
                     console.log('Results from dataset');
                     const { items } = await client.dataset(run.defaultDatasetId).listItems();
@@ -108,11 +90,11 @@ app.post("/upload", multer.single("img"), (req,res)=>{
                         res.redirect(item["file"])
                     });
                 })();
-    
+
                  //console.log(str1);
 
-                    
-              
+
+
                 }
               }
           });
@@ -121,7 +103,7 @@ app.post("/upload", multer.single("img"), (req,res)=>{
 
     })
 
-    
+
 })
 
 // app.post('/test' , (req,res)=>{
@@ -131,9 +113,9 @@ app.post("/upload", multer.single("img"), (req,res)=>{
 //         "query":req.body["text"],
 //         "lang": "en-US"
 //     };
-    
+
 //     (async () => {
-        
+
 //         const run = await client.actor("S84ymCuDy3DPGojAH").call(input);
 //         console.log('Results from dataset');
 //         const { items } = await client.dataset(run.defaultDatasetId).listItems();
@@ -142,13 +124,8 @@ app.post("/upload", multer.single("img"), (req,res)=>{
 //             res.redirect(item["file"])
 //         });
 //     })();
-    
+
 // });
 
 const server = app.listen(port ,()=>{
     console.log(`listining on ${port}`);
-});
-
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
-
